5d373200791591cb87941dfd769575776e175ac7
taaanmay <60434269+taaanmay@users.noreply.github.com>
2020-12-09 19:17:04 +0000

    Branch working. Employee Not tested

---
+++ Agency_Database.sql
@@ -0,0 +1,36 @@
+
+CREATE TABLE Branch
+(
+  Branch_ID INT NOT NULL,
+  Name CHAR(30) NOT NULL,
+  Phone_No INT NOT NULL,
+  Street_ CHAR(20),
+  Town_ CHAR(20),
+  County CHAR(20),
+  PRIMARY KEY (Branch_ID),
+  CONSTRAINT check_Phone_No CHECK ( Phone_No < 10000000000 )
+);
+
+
+Insert Into Branch Values(001, 'South Motors', 894111873, 'Honeypark', 'Dun Laoghaire', 'Dublin');
+Insert Into Branch Values(002, 'North Motors', 0894865585, 'Sword Centre', 'Sword', 'Dublin 9');
+/*
+Select * from branch;
+*/
+
+
+
+CREATE TABLE Employee
+(
+  Employee_ID INT NOT NULL,
+  FName VARCHAR(20) NOT NULL,
+  SName VARCHAR(20) NOT NULL,
+  Emp_Position VARCHAR(20) NOT NULL,
+  Emp_Status VARCHAR(20) NOT NULL,
+  Salary INT NOT NULL,
+  PPS CHAR(9) NOT NULL,
+  Branch_ID INT NOT NULL,
+  PRIMARY KEY (Employee_ID),
+  FOREIGN KEY (Branch_ID) REFERENCES Branch(Branch_ID),
+  CONSTRAINT PPS_Check CHECK(CHARACTER_LENGTH(PPS) = 9 AND SUBSTRING(PPS FROM 1 FOR 1) BETWEEN 'A' AND Z)
+);
